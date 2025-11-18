7bd9eb3d1de835c5d0c42ff44458ac18b2dffaf6
ckmonish2000 <ckmonish2000@gmail.com>
2025-11-18 20:26:07 +0000

    chore: mistral ocr test

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,10 +1,14 @@
-import MinioService from "minio";
+import MistralService from 'mistral'
+// import MinioService from "minio";
 
-const minioService = new MinioService();
-const bucketName = 'test-bucket'
-const objectName = 'random_1.jpeg'
-console.log(await minioService.getPresignedUrl({
-    bucketName,
-    objectName,
-    isFetch:true
-}))
+// const minioService = new MinioService();
+// const bucketName = 'test-bucket'
+// const objectName = 'random_1.jpeg'
+// console.log(await minioService.getPresignedUrl({
+//     bucketName,
+//     objectName,
+//     isFetch:true
+// }))
+
+const mistralCLient = new MistralService()
+mistralCLient.processImage()
--- packages/mistral/index.ts
+++ packages/mistral/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import { Mistral } from '@mistralai/mistralai';
+export default class MistralService{
+    private mistralClient
+
+    constructor(){
+        this.mistralClient = new Mistral({apiKey: process.env.MISTRAL_API_KEY });
+    }
+
+    async processImage(){
+        const ocrResponse = await this.mistralClient.ocr.process({
+            model: "mistral-ocr-latest",
+            document: {
+                type: "document_url",
+                documentUrl: "https://media.geeksforgeeks.org/wp-content/uploads/20210610151625/Hey21.png"
+            },
+            includeImageBase64: false
+        });
+        console.log(ocrResponse)
+    }
+}
